35cea85422e26c7232350cfd08f8d37e7366fc0a
Isaiah James <IsaiahTJames@gmail.com>
2025-02-20 22:01:01 -0700

    adjusted for 320px view

--- documentation/main.js
+++ documentation/main.js
@@ -1,11 +1,2 @@
 import './index.css'
 import 'flowbite'
-
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger-menu");
-    const navMenu = document.querySelector(".nav-menu");
-
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active"); // Toggle menu visibility
-    });
-});

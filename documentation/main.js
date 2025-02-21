02b7c9f77df7daf97ea083036ec1a354d8db3ed7
Isaiah James <IsaiahTJames@gmail.com>
2025-02-20 17:33:03 -0700

    updated the responsive css

--- documentation/main.js
+++ documentation/main.js
@@ -1,2 +1,11 @@
 import './index.css'
 import 'flowbite'
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger-menu");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active"); // Toggle menu visibility
+    });
+});
